bf1518d999bae5c3b82b5f03dea93b6a029c2d12
Chad Hinds <chadrickj8@gmail.com>
2020-01-14 19:37:44 -0500

    save the sort order as var and comment on it

--- javascript/sums.js
+++ javascript/sums.js
@@ -1,6 +1,7 @@
 let numbers = [54 ,10, 8, 12];
 
 function sumTwoSmallestNumbers(numbers) {
+    // Save the sort order as a variable
     nums = numbers.sort(function (a, b) { return a - b; });
     console.log(nums[0] + nums[1]);
 };
